1e7c4428b406ed2f046f02e925989e54087217a4
IrenePletneva <73946301+IrenePletneva@users.noreply.github.com>
2023-09-07 20:00:39 +0300

    fix allure

--- src/test/java/demoqa/test/RegistrationPageTest.java
+++ src/test/java/demoqa/test/RegistrationPageTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import demoqa.utils.TestsData;
 
 
-public class RegistrationPageTest extends RemoteTestBase {
+public class RegistrationPageTest extends BaseTest {
     RegistrationPage registrationPage = new RegistrationPage();
     TestsData testData = new TestsData();
 
